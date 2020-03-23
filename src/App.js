bfa411c00e128a559d5db2687685082242dce9e9
Jeremiah Stafford <52318268+jstafford74@users.noreply.github.com>
2020-03-23 06:27:24 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import HooksCakeContainer from './components/HooksCakeContainer';
+import NewCakeContainer from './components/NewCakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
+import ItemContainer from './components/ItemContainer';
+import UserContainer from './components/UserContainer';
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        {/* <ItemContainer cake />
+        <ItemContainer />
+
+        <HooksCakeContainer />
+        <IceCreamContainer />
+        <NewCakeContainer /> */}
+        <UserContainer />
+      </div>
+    </Provider>
+
   );
 }
 
